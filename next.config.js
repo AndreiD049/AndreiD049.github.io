e083b0bb25e35ccde1d383d237b40bea25e8679b
AndreiD049 <andrei.dimitrascu.94@gmail.com>
2021-11-19 15:58:23 +0200

    first

--- next.config.js
+++ next.config.js
@@ -2,7 +2,6 @@ const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
   reactStrictMode: true,
-  assetPrefix: isProd ? '/AndreiD049.github.io/' : '',
   webpack: (config) => {
     config.module.rules.push({
       test: /\.svg$/,
